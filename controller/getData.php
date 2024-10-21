7c0d1d002b6c9c7f70ab4db35441a97bb14277ef
luusytruong <lst27062004@gmail.com>
2024-10-21 21:35:54 +0700

    test api

---
+++ controller/getData.php
@@ -0,0 +1,29 @@
+<?php
+
+include __DIR__ . '/../model/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $table = isset($_GET['table']) ? $_GET['table'] : null;
+
+    if ($table) {
+        
+        $sql = "SELECT * FROM $table ORDER BY id ASC";
+        $stmt = $pdo->prepare( $sql );
+    
+        header( 'Content-Type: application/json; charset=UTF-8' );
+        
+        if ( $stmt->execute() ) {
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            // send json to client
+            echo json_encode( $data );
+        } else {
+            echo json_encode( [ 'error'=> 'failed to execute query' ] );
+        }
+    } else {
+        echo json_encode( [ 'error'=> 'table parameter is missing' ] );
+    }
+} else {
+    echo json_encode( [ 'error'=> 'invalid request method' ] );
+}
+
+?>
