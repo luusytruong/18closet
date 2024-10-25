de80ed0a6bdd3bb5ebc3aa124faf1193600392d0
luusytruong <lst27062004@gmail.com>
2024-10-25 21:45:24 +0700

    commit

--- model/createTables.php
+++ model/createTables.php
@@ -87,6 +87,18 @@ $sql = "CREATE TABLE IF NOT EXISTS carts (
 
 $pdo->exec( $sql );
 
+// Create table discounts
+$sql = "CREATE TABLE IF NOT EXISTS discounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+)";
+
+$pdo->exec( $sql );
+
 echo 'All tables have been created.';
 
 ?>
